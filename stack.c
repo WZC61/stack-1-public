945dda1ab6cb4dfc72ca2032863db7783c910f12
WZC61 <102889555+WZC61@users.noreply.github.com>
2022-05-01 01:54:17 +0800

    Update stack.c

--- stack.c
+++ stack.c
@@ -9,17 +9,62 @@
 
 struct stack *init_stack(struct stack *t)
 {
-	t->count = 0;
+ t->count = 0;
 
-	return t;
+ return t;
 }
 
 struct stack *create_stack()
 {
-	struct stack *t;
+ struct stack *t;
 
-	t = (struct stack *)malloc(sizeof(struct stack));
+ t = (struct stack *)malloc(sizeof(struct stack));
 
-	return init_stack(t);
+ return init_stack(t);
+}
+struct stack*push(struct stack*t,T e){
+    if(t->count >=MAX_ELE){
+        printf("栈是满的\n");
+        return NULL;
+    }else{
+        
+        t->store[t->count] = e;
+     t->count++;
+        return t;
+    }
+}
+bool is_empty(struct stack*t){
+    if(t->count == 0){
+        printf("栈是空的\n");
+        return 1;
+    }else{
+        printf("栈不是空栈\n");
+        return 0;
+
+    }
+}
+T pop(struct stack*t){
+    T a;
+    int loc;
+ assert(t->count > 0);
+
+ loc = t->count - 1;
+
+ a = t->store[loc];
+
+ t->count--;
+
+ return a;
+
+}
+T top( struct stack *t )
+{
+ int loc;
+
+ assert(t->count > 0);
+
+ loc = t->count - 1;
+
+ return t->store[loc];
 }
 
